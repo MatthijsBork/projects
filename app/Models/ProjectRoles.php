5977a598a7249dde2c016b986a03f1a087a2b0a9
unknown <mborkhuis@st.noorderpoort.nl>
2023-09-20 14:06:56 +0200

    migrations & models

---
+++ app/Models/ProjectRoles.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ProjectRoles extends Model
+{
+    use HasFactory;
+}
---
+++ database/migrations/2023_09_20_114207_create_project_user_roles_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('project_user_roles', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('role_id')->default(1);
+            $table->unsignedBigInteger('user_id')->default(1);
+            $table->unsignedBigInteger('project_id')->default(1);
+            $table->foreign('role_id')->references('id')->on('roles')->onDelete('cascade');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('project_id')->references('id')->on('projects')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('project_user_roles');
+    }
+};
