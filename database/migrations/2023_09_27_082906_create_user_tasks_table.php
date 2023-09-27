02c3c164e554e5df08e5914a67a69a5871afbe61
unknown <mborkhuis@st.noorderpoort.nl>
2023-09-27 11:14:36 +0200

    fixes

--- database/migrations/2023_09_27_082906_create_user_tasks_table.php
+++ database/migrations/2023_09_27_082906_create_user_tasks_table.php
@@ -15,9 +15,9 @@ return new class extends Migration
     {
         Schema::create('user_tasks', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('role_id')->default(1);
             $table->unsignedBigInteger('user_id')->default(1);
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->unsignedBigInteger('task_id')->default(1);
             $table->foreign('task_id')->references('id')->on('tasks')->onDelete('cascade');
         });
     }
