156570f7315223e854eb819988f9edf7c8e584a9
unknown <mborkhuis@st.noorderpoort.nl>
2023-10-04 14:24:04 +0200

    button components

---
+++ resources/views/components/cancel-button.blade.php
@@ -0,0 +1,4 @@
+<button type="button" onclick="history.back()"
+    class="px-4 py-2 mt-3 text-red-700 bg-transparent rounded-lg hover:bg-red-500 hover:text-white hover:border-transparent">
+    {{ $slot }}
+</button>
---
+++ resources/views/components/submit-button.blade.php
@@ -0,0 +1,1 @@
+<button type="submit" class="px-4 py-2 text-white bg-blue-700 rounded-lg hover:bg-blue-800">{{ $slot }}</button>
